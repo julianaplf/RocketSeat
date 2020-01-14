e2d1d055263a9104c00e751f0b68c831e87787ce
julianaplf <juh.fonseca@gmail.com>
2020-01-14 06:52:18 -0300

    aula sobre classes

--- cursoES6/main.js
+++ cursoES6/main.js
@@ -1,7 +1,35 @@
-alert('oi');
-class Teste{
-    metodo(){
+// criei classe somente para testar o extends
 
+class List {
+    // construtor da classe
+    constructor() {
+        this.data = [];
     }
-    outro2(){}
+    // metodo da classe
+    add(data) {
+        this.data.push(data);
+    }
+    renderTodos(){
+        var listEl = document.querySelector('#list');
+        listEl.innerHTML = '';
+        var todos = this.data;
+        for(var i=0; i<todos.length; i++){
+            var liEl = document.createElement('li');
+            liEl.appendChild(document.createTextNode(todos[i]));
+            listEl.appendChild(liEl);
+        }
+    }
+}
+
+class TodoList extends List { }
+// devo primeiro declarar a classe para somente depois chamar o construtor dela
+//pois com JS não tem Hoisting (colocar no topo funcoes e variaveis)
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    var inputEl = document.querySelector('#inputTodo');
+    MinhaLista.add(inputEl.value);
+    inputEl.value = '';
+    MinhaLista.renderTodos();
 }
