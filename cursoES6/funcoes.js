7e5b739b35a753d935bb18445aaf3c784139f887
Fonseca <juliana.p.fonseca@accenture.com>
2020-01-20 18:32:10 -0300

    exercicio webpack

--- cursoES6/funcoes.js
+++ cursoES6/funcoes.js
@@ -1,3 +1,13 @@
-export function soma(a,b){
+/* export function soma(a,b){
     return a + b;
+}
+export function sub(a,b){
+    return a - b;
+} */
+// exercicios 
+export const idade = 23;
+export default class Usuario {
+ static info() {
+    console.log('Apenas teste');
+ }
 }
