63735756c969ce57a8753bd75cc34c89c9840c47
julianaplf <juh.fonseca@gmail.com>
2020-06-23 22:30:32 -0300

    rotas, database connection

---
+++ NLW/server/src/routes.ts
@@ -0,0 +1,12 @@
+
+import express, { RouterOptions } from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+   return res.json({
+       message : 'hello world'
+   });
+});
+
+export default router;
--- NLW/server/src/server.ts
+++ NLW/server/src/server.ts
@@ -1,26 +1,8 @@
 import express, { response, json } from 'express';
+import routes from './routes';
 
 const app = express();
-
-const users = ['Diego', 'Juliana', 'Gustavo', 'Pretinha'];
-
-app.get('/users/:id', (req, res) => {
-    const id = Number(req.params.id);
-    res.json(users[id]);
-});
-
-app.get('/users', (req, res) => {
-    const search = String(req.query.search);
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-    return res.json(filteredUsers)
-});
-
-app.post('/users', (req, res) => {
-    const user = {
-        name : 'Juliana', 
-        email: 'juliana.fonseca@gmail.com'
-    }
-    return res.json(user);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
