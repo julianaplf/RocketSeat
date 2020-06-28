30fa7c0ad5dfcb74ba72c2dbc07c7242be6fc3b0
julianaplf <juh.fonseca@gmail.com>
2020-06-27 21:04:44 -0300

    criacao de algumas rotas

---
+++ NLW/server/src/controllers/PointsController.ts
@@ -0,0 +1,76 @@
+import {Request, Response} from 'express';
+import knex from '../database/connection';
+
+class PointsController{
+    async create(request: Request, response: Response) {
+        const {
+          name,
+          email,
+          whatsapp,
+          latitude,
+          longitude,
+          city,
+          uf,
+          items
+        } = request.body;
+    
+        const trx = await knex.transaction();
+    
+        const point = {
+          image: 'fake-img',
+          name,
+          email,
+          whatsapp,
+          latitude,
+          longitude,
+          city,
+          uf
+        };
+      
+        const insertedIds = await trx('points').insert(point);
+      
+        const point_id = insertedIds[0];
+      
+        const pointItems = items
+          .split(',')
+          .map((item: string) => Number(item.trim()))
+          .map((item_id: number) => {
+            return {
+              item_id,
+              point_id,
+            };
+          })
+      
+        await trx('point_items').insert(pointItems);
+    
+        await trx.commit();
+      
+        return response.json({ 
+          id: point_id,
+          ...point,
+        });
+      }
+    async show(request: Request, response: Response) {
+        const { id } = request.params;
+    
+        const point = await knex('points').where('id', id).first();
+    
+        if (!point) {
+          return response.status(400).json({ message: 'Point not found.' });
+        }
+    
+        const serializedPoint = { 
+          ...point,
+          image_url: `http://localhost:3333/uploads/${point.image}`,
+        };
+    
+        const items = await knex('items')
+          .join('point_items', 'items.id', '=', 'point_items.item_id')
+          .where('point_items.point_id', id)
+          .select('items.title');
+    
+        return response.json({ point: serializedPoint, items });
+      }
+}
+
+export default PointsController;
--- NLW/server/src/routes.ts
+++ NLW/server/src/routes.ts
@@ -1,12 +1,19 @@
 
 import express, { RouterOptions } from 'express';
-
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 const router = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 router.get('/', (req, res) => {
    return res.json({
        message : 'hello world'
    });
 });
+
+router.get('/items', itemsController.index);
+router.post('/points', pointsController.create);
+router.get('/points/:id', pointsController.show);
 
 export default router;
